cbbc2543febbabd448a4739b3c048383e6e1520d
Hobab <hobabhbk@yahoo.com>
2023-09-14 21:35:06 +0500

    testing api after deployment

--- front-end/src/utils/axios.instance.js
+++ front-end/src/utils/axios.instance.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: 'todo-backend-9cyp.onrender.com',
-    headers: {
-    "Access-Control-Allow-Origin": 'https://melodious-sorbet-9c8d04.netlify.app/'
-    },
     withCredentials: true,
     });
   
